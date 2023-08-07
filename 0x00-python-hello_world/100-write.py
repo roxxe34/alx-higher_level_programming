0787f070033ebcec57a78967974597f1e83a6748
roxxe34 <hamzafarissi7@gmail.com>
2023-08-07 07:07:22 -0700

    done

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 import sys
-def write_to_stderr(message):
-        sys.stderr.write(message)
-write_to_stderr('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 exit(1)
