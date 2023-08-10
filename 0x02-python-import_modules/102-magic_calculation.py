b8a2e46b6cab7071a5e9cc0ea69ea29a6ccc8e6a
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-08-10 21:25:23 +0100

    Update 102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-def magic_function(a, b):
+def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
+
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
+        return (c)
+
     else:
-        c = sub(a, b)
-    return c
+        return(sub(a, b))
