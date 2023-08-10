3b8ecc2f2da05768359e7e45cbea5cbcbe7c381a
roxxe34 <hamzafarissi7@gmail.com>
2023-08-10 13:41:05 -0700

    done

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+from abcd import alpha
+alpha()
---
+++ 0x02-python-import_modules/abcd.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def alpha():
+    for i in range(65, 91):
+        print("{}".format(chr(i)), end="")
+    print("")
