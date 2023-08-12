71f1da1265813fda79d02134079558d0aab53559
roxxe34 <hamzafarissi7@gmail.com>
2023-08-12 18:20:55 +0100

    done

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,6 @@ def replace_in_list(my_list, idx, element):
     if not 0 <= idx <= len(my_list):
         return my_list
     else:
+        my_list.remove(idx)
         my_list.insert(idx, element)
         return my_list
