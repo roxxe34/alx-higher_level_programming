6cb7eb2cbff48ef1b0f3f06c2ec2da8a9d1efa5e
roxxe34 <hamzafarissi7@gmail.com>
2023-08-16 23:41:03 +0100

    done

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    keys_to_remove = []
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
     for k, v in a_dictionary.items():
-        if v == value:
-            keys_to_remove.append(k)
-    for key in keys_to_remove:
-        a_dictionary.pop(key)
-
-    return a_dictionary
+        new_dictionary[k] = a_dictionary[v] * 2
