33f58165783526ab23c52ec4356ac4cc416e5933
roxxe34 <hamzafarissi7@gmail.com>
2023-08-16 21:34:16 +0100

    done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,13 +2,8 @@
 def roman_to_int(roman_string):
     if not (isinstance(roman_string, str)) or roman_string is None:
         return 0
-    roman_dict = {'I': 1,
-                'V': 5,
-                'X': 10,
-                'L': 50,
-                'C': 100,
-                'D': 500,
-                'M': 1000}
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
+                  'M': 1000}
 
     rom_list = [roman_dict[char] for char in roman_string]
 
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -4,4 +4,3 @@ def multiply_by_2(a_dictionary):
     for k, v in a_dictionary.items():
         new_dictionary[k] = a_dictionary[k] * 2
     return new_dictionary
-        
