4a11d892d82415411f13197ead375ca569935bf6
roxxe34 <hamzafarissi7@gmail.com>
2023-08-16 21:44:49 +0100

    done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,7 @@ def roman_to_int(roman_string):
         return 0
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                   'M': 1000}
-
     rom_list = [roman_dict[char] for char in roman_string]
-
     final_number = 0
     i = 0
     while(i < len(rom_list)):
