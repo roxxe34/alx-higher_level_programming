f4707879a487c87562be31ec505e39187a575713
roxxe34 <hamzafarissi7@gmail.com>
2023-11-19 20:07:29 +0100

    done

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa """
+""" Script that lists all values in the states table of hbtn_0e_0_usa
+where name matches the argument """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="127.0.0.1", port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     curr = db.cursor()
-    curr.execute(f"SELECT * FROM states "
-                 f"WHERE name = '{sys.argv[4]}'  ORDER BY id ASC")
+    curr.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                 (sys.argv[4],))
     data = curr.fetchall()
     for row in data:
         print(row)
+    curr.close()
+    db.close()
