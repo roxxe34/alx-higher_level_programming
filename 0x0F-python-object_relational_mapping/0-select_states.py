b5a3fec9a8c42191f333c8e7ee8b429ff971ccc0
roxxe34 <hamzafarissi7@gmail.com>
2023-11-19 19:22:03 +0100

    done

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,33 +3,14 @@
 List all states from a MySQL db on localhost at port 3306
 """
 
-from mysqlman import MySQLMan
-from MySQLdb import Error
-from sys import argv, exit, stderr
+import sys
+import MySQLdb
 
 
-HELP = '{} username password database'.format(argv[0])
-HOST = 'localhost'
-PORT = 3306
-
-
-if __name__ == '__main__':
-    try:
-        mysqlman = MySQLMan(
-            connect=True,
-            user=argv[1],
-            password=argv[2],
-            database=argv[3],
-            host=HOST,
-            port=PORT,
-        )
-    except IndexError:
-        stderr.write('usage: {}\n'.format(HELP))
-        exit(2)
-    except Error as e:
-        stderr.write('{}\n'.format(e.args[1]))
-        exit(1)
-    query = "SELECT id, name FROM states ORDER BY id;"
-    results = mysqlman.query([query, ()])
-    for row in results[0]:
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    curr = db.cursor()
+    curr.execute("SELECT * FROM states ORDER BY states.id ASC")
+    data = curr.fetchall()
+    for row in data:
         print(row)
