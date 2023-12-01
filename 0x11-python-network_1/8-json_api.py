057b452a699a649cf4e5fe6a03573c3594f43e54
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 16:40:21 +0100

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""Python script that takes in a letter and sends a POST request"""
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
 import sys
 import requests
 import json
