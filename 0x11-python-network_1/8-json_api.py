b3f08896826c19099e167a5d9ff798d446f31946
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 16:01:50 +0100

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,7 +4,9 @@ import sys
 import requests
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    letter = sys.argv[2]
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1]
     r = requests.post(url, data={'q': letter})
-    print(r.text)
+    print(r.text['id'])
+    print(r.text['name'])
+
