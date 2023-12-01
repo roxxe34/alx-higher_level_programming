6f35840899d1cf47b8859946fadca2cca60ad0b8
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 19:19:43 +0100

    done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest) of the repository rails"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    r = requests.get(
+        f"https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits")
+    data = r.json()
+    for d in range(10):
+        print("{}: {}".format(data[d]['sha'],
+                              data[d]["commit"]["author"]["name"]))
