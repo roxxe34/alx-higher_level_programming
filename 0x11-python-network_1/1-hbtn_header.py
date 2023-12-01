e03ac2b477bd9ff8b070dc751e1a15a66d74238d
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 01:32:50 +0100

    done

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and displays"""
+import urllib.parse
+import urllib.request
+import sys
+
+url = sys.argv[1]
+value = sys.argv[2]
+
+data = urllib.parse.urlencode(value)
+data = data.encode("utf-8")
+req = urllib.request.Request(url, data)
+with urllib.request.urlopen(req) as response:
+    the_page = response.read()
