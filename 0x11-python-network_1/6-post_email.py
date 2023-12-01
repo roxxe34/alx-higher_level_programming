35f432c5b09dcf52017c4f4254a1a38955877642
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 14:55:50 +0100

    done

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Python script that takes in a URL and an email address, sends a POST request"""
+""" Python script that takes in a URL and an email,sends a POST request"""
 import sys
 import requests
 
