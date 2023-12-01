dd3338eeaa3a801ada80cb5e357fc57ec91b1a88
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 13:33:23 +0100

    done

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Python script that takes in a URL, sends a request to the URL and displays body"""
+import sys
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as f:
+            body = f.read()
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(e.code)
