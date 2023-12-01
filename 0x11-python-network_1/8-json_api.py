f21cc899527f81af9e0fc0a96c58e9d20da0efee
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 16:06:39 +0100

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,10 +2,12 @@
 """Python script that takes in a letter and sends a POST request"""
 import sys
 import requests
+import json
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
     letter = sys.argv[1]
     r = requests.post(url, data={'q': letter})
-    print(r.text)
-    print(type(r.text))
+    data = r.text
+    data_dict = json.loads(data)
+    print("[{}] {}".format(data_dict['id'], data_dict['name']))
