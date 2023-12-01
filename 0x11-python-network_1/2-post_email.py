cb0f1ee73259f4f0fbde2cbd7d81f7260618bbb7
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 04:08:50 -0800

    done

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to the URL and displays"""
+"""Python script that takes in a URL, sends a request to the URL and disp"""
 import urllib.parse
 import urllib.request
 import sys
