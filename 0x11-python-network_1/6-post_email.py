790c3cf54557784503a92ff146b303742559cca8
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 14:53:00 +0100

    done

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL and an email address, sends a POST request"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data={'email': email})
