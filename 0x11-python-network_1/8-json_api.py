4f9a2ef903643814826b29d5849009454f8f733d
roxxe34 <hamzafarissi7@gmail.com>
2023-12-01 16:37:47 +0100

    done

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,17 +6,14 @@ import json
 
 if __name__ == '__main__':
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) < 1:
-        print("No result")
-    else:
-        try:
-            letter = sys.argv[1]
-            r = requests.post(url, data={'q': letter})
-            data = r.text
-            data_dict = json.loads(data)
-            if len(data_dict) == 0:
-                print("No result")
-            else:
-                print("[{}] {}".format(data_dict['id'], data_dict['name']))
-        except json.JSONDecodeError:
-            print("Not a valid JSON")
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    r = requests.post(url, data={'q': letter})
+    data = r.text
+    try:
+        data_dict = json.loads(data)
+        if len(data_dict) == 0:
+            print("No result")
+        else:
+            print("[{}] {}".format(data_dict['id'], data_dict['name']))
+    except json.JSONDecodeError:
+        print("Not a valid JSON")
